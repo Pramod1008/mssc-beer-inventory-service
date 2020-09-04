f70ace1216981f6661bc2262022302a93d875ee8
pramod <pramod@open-insights.com>
2020-09-04 18:49:30 +0530

    update to use snapshot Bom closes #2

--- src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
+++ src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class BeerInvntoryBootstrap implements CommandLineRunner {
+public class BeerInventoryBootstrap implements CommandLineRunner {
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
@@ -51,7 +51,7 @@ public class BeerInvntoryBootstrap implements CommandLineRunner {
                 .builder()
                 .beerId(BEER_3_UUID)
                 .upc(BEER_3_UPC)
-                .quantityOnHand(50)
+                .quantityOnHand(40)
                 .build());
 
         log.debug("Loaded Inventory. Record count: " + beerInventoryRepository.count());
